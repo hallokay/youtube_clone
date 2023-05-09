e4ec4d25f442737644eaafa4494f5009f31d6c95
hallokay <hallokay044@gmail.com>
2023-05-10 00:14:25 +0900

    HHH

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
 import { Routes, Route } from "react-router-dom";
 import { Layout, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./Pages";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" index element={<Feed />} />
-        <Route path="video/:id" element={<VideoDetail />} />
-        <Route path="channel/:id" element={<ChannelDetail />} />
-        <Route path="search/:searchTerm" element={<SearchFeed />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" index element={<Feed />} />
+          <Route path="video/:id" element={<VideoDetail />} />
+          <Route path="channel/:id" element={<ChannelDetail />} />
+          <Route path="search/:searchTerm" element={<SearchFeed />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
